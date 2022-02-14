21402ce463d47432309e7817f2ae16ebb5506681
Jaume Parrot Altisent <jparrot@gps-plan.com>
2022-02-14 09:04:53 +0100

    Verificar valor en una etiqueta HTML

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -2,11 +2,23 @@ import { shallowMount } from '@vue/test-utils'
 import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
-  test('debe de hacer match con el snaphot', () => {
+  // test('debe de hacer match con el snaphot', () => {
+
+  //   const wrapper = shallowMount(Counter)
+
+  //   expect(wrapper.html()).toMatchSnapshot()
+
+  // })
+
+  test('h2 debe de tener el valor por defecto "Counter"', () => {
 
     const wrapper = shallowMount(Counter)
 
-    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.find('h2').exists()).toBeTruthy()
+
+    const h2Value = wrapper.find('h2').text()
+
+    expect(h2Value).toBe('Counter')
 
   })
 })
