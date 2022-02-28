503e288a6b843e662a95bc7a4798e7c420932d33
Jaume Parrot Altisent <jparrot@gps-plan.com>
2022-02-28 08:37:51 +0100

    Simular eventos

--- tests/unit/components/counter.spec.js
+++ tests/unit/components/counter.spec.js
@@ -2,6 +2,13 @@ import { shallowMount } from '@vue/test-utils'
 import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
+
+  let wrapper
+
+  beforeEach(()=>{
+    wrapper = shallowMount(Counter)
+  })
+
   // test('debe de hacer match con el snaphot', () => {
 
   //   const wrapper = shallowMount(Counter)
@@ -11,8 +18,6 @@ describe('Counter Component', () => {
   // })
 
   test('h2 debe de tener el valor por defecto "Counter"', () => {
-
-    const wrapper = shallowMount(Counter)
 
     expect(wrapper.find('h2').exists()).toBeTruthy()
 
@@ -24,9 +29,6 @@ describe('Counter Component', () => {
 
   test('el calor por defecto debe de ser 100 en el p', () =>{
 
-    // Wrapper
-    const wrapper = shallowMount(Counter)
-
     // pTags
     // const pTags = wrapper.findAll('p')
     const value = wrapper.find('[data-testid="counter"]').text()
@@ -34,6 +36,22 @@ describe('Counter Component', () => {
     // expect segundo p === 100
     // expect(pTags[1].text()).toBe('100')
     expect(value).toBe('100')
+  })
+
+  test('debe de incrementar y decrementar el contador', async () =>{
+
+    const [ increaseBtn, decreaseBtn ] = wrapper.findAll('button')
+
+    await increaseBtn.trigger('click')
+    await increaseBtn.trigger('click')
+    await increaseBtn.trigger('click')
+    await decreaseBtn.trigger('click')
+    await decreaseBtn.trigger('click')
+
+    const value = wrapper.find('[data-testid="counter"]').text()
+
+    expect(value).toBe('101')
+
   })
 
 
