fb111937d86077c8e0da1897c0a5aab9394bfdc3
Jaume Parrot Altisent <jparrot@gps-plan.com>
2022-03-19 19:27:58 +0100

    Spy y Mocks

--- tests/unit/components/indecision.spec.js
+++ tests/unit/components/indecision.spec.js
@@ -4,16 +4,25 @@ import Indecision from '@/components/Indecision'
 describe('Indecision Component', () => {
 
   let wrapper
+  let clgSpy
 
   beforeEach(()=>{
     wrapper = shallowMount(Indecision)
+
+    clgSpy = jest.spyOn(console, 'log')
+
   })
 
   test('debe de hacer match con el snaphot', () => {
     expect(wrapper.html()).toMatchSnapshot()
   })
 
-  test('escribir en el input no debe de disparar nada (console.log)', () => {
+  test('escribir en el input no debe de disparar nada (console.log)', async() => {
+
+    const input = wrapper.find('input')
+    await input.setValue('Hola Mundo')
+
+    expect(clgSpy).toHaveBeenCalledTimes(1)
 
   })
 
